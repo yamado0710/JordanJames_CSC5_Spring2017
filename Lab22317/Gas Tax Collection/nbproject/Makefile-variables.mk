b4803b95ecad50130409b25ae5c39ed8b4beb905
WhosJohnGault <Bravesboy1116@gmail.com>
2017-02-27 11:49:20 -0800

    Add files via upload

---
+++ Lab22317/Gas Tax Collection/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=gas_tax_collection
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/gas_tax_collection
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=gastaxcollection.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/gastaxcollection.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=gas_tax_collection
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/gas_tax_collection
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=gastaxcollection.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/gastaxcollection.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
