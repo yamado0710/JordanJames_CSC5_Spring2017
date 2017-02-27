72fe9737bb180666c9fc98c7f90e70ea6c9c2601
WhosJohnGault <Bravesboy1116@gmail.com>
2017-02-27 00:33:09 -0800

    Update main.cpp

--- Gas Tax Collection/main.cpp
+++ Gas Tax Collection/main.cpp
@@ -21,28 +21,32 @@ const float PERCENT=100.0f;
 //Execution begins here
 int main(int argc, char** argv) {
     
-    //Declare variables
-    float bsprce=
-    float fedTaxg=0.184f; //8.4 cents per gallon
-    float caTaxg=0.390f;  //39 cents per gallon state tax
-    float cptTaxg=0.100f; //10 cents per gallon
-    float sstTaxg=0.08f;   //state sale tax 8 percent
-    float ocPrfg=0.07f; //Oil Company profit/Gallon      
-    float pumpPr,basePr;
-    float pctTax,pctPrft;
-    
-    //Initialize variables
-    pp-sstTaxg-cptTaxg-fedTaxg=bsprce*(1)
+    //Declare and initialize variables
+    float fedTax=0.184f; //Federal Tax 8.4 cents per gallon
+    float caTax=0.390f;  //California excise tax 39 cents per gallon state tax
+    float cpTax=0.100f; //California cap and trade tax 10 cents per gallon
+    float caSlsTx=0.08f;   //California state sale tax 8 percent
+    float oilPrft=0.07f; //Oil Company profit in cents per gallon      
+    float pumpPr,basePr; //Pump price and base price in $'s
+    float pctTax,pctPrft; //Percentage tax, Percentage proft
+
     //Input Data
     cout<<"what price did you pay at the pump $'s/gallon"<<endl;
     cin>>pumpPr;
     
     //Map inputs to outputs or process the data
-    bsprce=(pumpPr-fedTaxg-caTaxg-cptTax)/(1+sstTaxg);
-    pctTax=(bsprce*sstTaxg+fedTaxg+caTaxg+cptTaxg);
-    pctPrft=ocPrfg/bsprce*PERCENT;
+    basePr=(pumpPr-fedTax-caTax-cpTax)/(1+caSlsTx);
+    pctTax=(basePr*caSlsTx+fedTax+caTax+cpTax)/basePr*PERCENT; 
+    pctPrft=ocPrfg/bsprce*PERCENT; 
     //outputs the transformed data
-     
+     cout<<"Federal tax/gallon           = $"<<fedTax<<endl;
+     cout<<"California tax/gallon        = $"<<caTax<<endl;
+     cout<<"Cap and trade tax/gallon     = $"<<cpTax<<endl;
+     cout<<"California sales tax/gallon  = $"<<caSlsTx*basePr<<endl;
+     cout<<"Base Price/gallon            = $"<<basePr<<endl;
+     cout<<"Oil company profit/gallon    = $"<<pctPrft<<"%"<<endl;
+     cout<<"Total percentage tax/Gallon  = $"<<pctTax<<"%"<<endl;
+      
     //Exit stage right!
 return 0;
 }
